2ac5cca7556eef6db9c432d15ee47bbd3048d8e1
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2017-05-04 00:34:47 +0530

    :tada: Basic architecture created

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="app">
+    <h1 class="title">Vue Lightbox</h1>
+    <h4 class="desc">A wrapper around Simple Lightbox for Vue 2</h4>
+    <Vuelightbox id="mylightbox"></Vuelightbox>
+</div>
+</template>
+
+<script>
+import Vuelightbox from '../src/index'
+export default {
+  name: 'MainApp',
+  components: {
+    Vuelightbox
+  },
+  mounted() {
+    console.log("App Mounted");
+  }
+}
+</script>
+
+<style scoped>
+@import 'https://fonts.googleapis.com/css?family=Roboto';
+</style>
---
+++ src/index.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+  export default{
+    props:{
+      id : {
+        type :  String,
+        required : true
+      }
+    },
+    mounted(){
+      try {
+          window.$ = window.jQuery = require('jquery');
+          var simplelightbox = require('simplelightbox');
+      } catch (e) {}
+    }
+  }
+</script>
+
+<style scoped></style>
