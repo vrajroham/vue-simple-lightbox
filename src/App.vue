62f0f2591518d693a56e7d5aeb2f01abe54051b2
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2017-05-04 19:36:49 +0530

    :tada: parent overriding css of child

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-<div id="app">
-    <h1 class="title">Vue Lightbox</h1>
-    <h4 class="desc">A wrapper around Simple Lightbox for Vue 2</h4>
-    <Vuelightbox id="mylightbox"></Vuelightbox>
-</div>
+  <div id="app">
+      <h1 class="title">Vue Lightbox</h1>
+      <h4 class="desc">A wrapper around Simple Lightbox for Vue 2</h4>
+      <Vuelightbox id="mylightbox" :items="items" img_class="imgs df"></Vuelightbox>
+  </div>
 </template>
 
 <script>
@@ -13,12 +13,22 @@ export default {
   components: {
     Vuelightbox
   },
+  data(){
+    return{
+      items : ['http://andreknieriem.de/sl/demo/images/thumbs/thumb1.jpg','http://andreknieriem.de/sl/demo/images/thumbs/thumb2.jpg','http://andreknieriem.de/sl/demo/images/thumbs/thumb3.jpg']
+    }
+  },
   mounted() {
     console.log("App Mounted");
+    console.log(this.items);
   }
 }
 </script>
 
-<style scoped>
+<style>
 @import 'https://fonts.googleapis.com/css?family=Roboto';
+
+.imgs.df{
+  padding-left: 20px;
+}
 </style>
--- src/index.vue
+++ src/index.vue
@@ -1,4 +1,7 @@
 <template>
+  <ul>
+    <img :src="i" v-for="i in items" alt="" :class="img_class">
+  </ul>
 </template>
 
 <script>
@@ -7,9 +10,16 @@
       id : {
         type :  String,
         required : true
+      },
+      items : {
+        type : Array
+      },
+      img_class : {
+        type : String
       }
     },
     mounted(){
+      console.log(this.items);
       try {
           window.$ = window.jQuery = require('jquery');
           var simplelightbox = require('simplelightbox');
