a1ba323fb62950f73408f5320fb5abe150efd1ac
Vaibhavraj <vaibhavraj.developer@gmail.com>
2017-05-05 01:22:32 +0530

    Sample plugin implemented

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,44 @@
 <template>
   <div id="app">
-      <h1 class="title">Vue Lightbox</h1>
-      <h4 class="desc">A wrapper around Simple Lightbox for Vue 2</h4>
-      <Vuelightbox id="mylightbox" :items="items" img_class="imgs df"></Vuelightbox>
+      <Vuelightbox id="mylightbox" :images="items"></Vuelightbox>
   </div>
 </template>
 
 <script>
-import Vuelightbox from '../src/index'
-export default {
-  name: 'MainApp',
-  components: {
-    Vuelightbox
-  },
-  data(){
-    return{
-      items : ['http://andreknieriem.de/sl/demo/images/thumbs/thumb1.jpg','http://andreknieriem.de/sl/demo/images/thumbs/thumb2.jpg','http://andreknieriem.de/sl/demo/images/thumbs/thumb3.jpg']
+  import Vuelightbox from '../src/index'
+  export default {
+    name: 'MainApp',
+    components: {
+      Vuelightbox
+    },
+    data(){
+      return{
+        items : [
+          {
+              src : 'http://andreknieriem.de/sl/demo/images/thumbs/thumb1.jpg',
+              title : 'Image 2'
+          },
+          {
+              src : 'http://andreknieriem.de/sl/demo/images/thumbs/thumb2.jpg',
+              title : 'Image 3'
+          },
+          {
+              src : 'http://andreknieriem.de/sl/demo/images/thumbs/thumb3.jpg',
+              title : ''
+          },
+          {
+              src : 'http://andreknieriem.de/sl/demo/images/thumbs/thumb4.jpg',
+              title : ''
+          },
+        ]
+      }
+    },
+    mounted() {
+
     }
-  },
-  mounted() {
-    console.log("App Mounted");
-    console.log(this.items);
   }
-}
 </script>
 
 <style>
-@import 'https://fonts.googleapis.com/css?family=Roboto';
 
-.imgs.df{
-  padding-left: 20px;
-}
 </style>
--- src/index.vue
+++ src/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <ul>
-    <img :src="i" v-for="i in items" alt="" :class="img_class">
-  </ul>
+  <div class="container">
+    <div class="gallery" v-for="image,key in images">
+      <a :href="image.src" :class="{'big' : key == 0}"><img :src="image.src" alt="" :title="image.title" /></a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,7 +13,7 @@
         type :  String,
         required : true
       },
-      items : {
+      images : {
         type : Array
       },
       img_class : {
@@ -23,9 +25,97 @@
       try {
           window.$ = window.jQuery = require('jquery');
           var simplelightbox = require('simplelightbox');
+          var gal = $('.gallery a').simpleLightbox()
       } catch (e) {}
     }
   }
 </script>
 
-<style scoped></style>
+<style>
+/*  a img {
+    float: left;
+    width: 20%;
+    height: auto;
+    border: 2px solid #fff;
+    -webkit-transition: -webkit-transform .15s ease;
+    -moz-transition: -moz-transform .15s ease;
+    -o-transition: -o-transform .15s ease;
+    -ms-transition: -ms-transform .15s ease;
+    transition: transform .15s ease;
+    position: relative;
+  }
+
+  a:hover img {
+    -webkit-transform: scale(1.05);
+    -moz-transform: scale(1.05);
+    -o-transform: scale(1.05);
+    -ms-transform: scale(1.05);
+    transform: scale(1.05);
+    z-index: 5;
+  }*/
+
+
+  /* line 8, ../sass/demo.scss */
+  * {
+    box-sizing: border-box;
+  }
+
+  /* line 12, ../sass/demo.scss */
+  a {
+    color: #4ab19a;
+  }
+
+  /* line 16, ../sass/demo.scss */
+  .clear {
+    clear: both;
+    float: none;
+    width: 100%;
+  }
+
+  /* line 22, ../sass/demo.scss */
+  .container {
+    max-width: 1170px;
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  /* line 32, ../sass/demo.scss */
+  .container .gallery a img {
+    float: left;
+    width: 20%;
+    height: auto;
+    border: 2px solid #fff;
+    -webkit-transition: -webkit-transform .15s ease;
+    -moz-transition: -moz-transform .15s ease;
+    -o-transition: -o-transform .15s ease;
+    -ms-transition: -ms-transform .15s ease;
+    transition: transform .15s ease;
+    position: relative;
+  }
+  /* line 46, ../sass/demo.scss */
+  .container .gallery a:hover img {
+    -webkit-transform: scale(1.05);
+    -moz-transform: scale(1.05);
+    -o-transform: scale(1.05);
+    -ms-transform: scale(1.05);
+    transform: scale(1.05);
+    z-index: 5;
+  }
+  /* line 57, ../sass/demo.scss */
+  .container .gallery a.big img {
+    width: 40%;
+  }
+
+  /* line 65, ../sass/demo.scss */
+  .align-center {
+    text-align: center;
+  }
+
+
+
+  @import url('https://fonts.googleapis.com/css?family=Ruda');
+  @import "~simplelightbox/dist/simplelightbox.css";
+
+</style>
